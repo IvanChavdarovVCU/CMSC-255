9cd6cb77d49a43c334d74652592fe4873155e5a9
IvanChavdarovVCU <112003717+IvanChavdarovVCU@users.noreply.github.com>
2022-08-24 12:12:45 -0400

    first program

---
+++ Hello.java
@@ -0,0 +1,21 @@
+/************************************************************************	
+* Hello.java
+*************************************************************************
+* Ivan Chavdarov
+* 08/24/22
+* CMSC 255-004
+*************************************************************************	
+*/
+public class Hello {
+	public static void main(String[] args) {
+
+	System.out.println("<Ivan Chavdarov>");
+	System.out.println("<Hello.java>");
+	System.out.println("<Lab1 first program>");
+	System.out.println("<08/24/22>");
+	System.out.println("<CMSC 255-004>");
+	System.out.println();
+
+
+	}
+}
